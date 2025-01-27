4e10f9605488c9c653fe1b452ad2687fb148d1bb
Isaac Fung <Isaac Fung>
2025-01-27 01:08:11 -0500

    feat: directory reorgnize

--- src/app/pages/api/artists.ts
+++ src/app/pages/api/artists.ts
@@ -2,16 +2,23 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios';
 
 export default async function getUserArtists(req: NextApiRequest, res: NextApiResponse) {
-    const userArtists = await axios.get('https://api.spotify.com/v1/me/top/artists', {
+    const userArtistsResponse = await axios.get('https://api.spotify.com/v1/me/top/artists', {
         params: {
-            limit: 100,
+            limit: 50,
             time_range: 'long_term',
             offset: 0
         },
         headers: {
             'Authorization': 'Bearer ' + req.query.access_token
         }
-    })
+    })  
 
+    const userArtists = userArtistsResponse.data
+
+    var artistCache = {}
+    userArtists.artists.forEach(artist => {
+        artistCache[artist.name] = artist.popularity
+    })
+    
 
 }
