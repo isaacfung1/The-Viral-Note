07209ed3a0d490a155997223116a5db4a3fbfdd1
Isaac <fsaaciung@gmail.com>
2025-01-29 20:25:47 -0500

    fix: redundant routing

---
+++ src/pages/api/login.ts
@@ -0,0 +1,25 @@
+"use client";
+import querystring from "querystring";
+import crypto from "crypto";
+
+function generateRandomString(length: number) {
+  return crypto
+    .randomBytes(Math.ceil(length / 2))
+    .toString("hex")
+    .slice(0, length);
+}
+
+
+export default function redirect_to_spotify() {
+  const params = {
+    response_type: "code",
+    client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+    scope: "user-read-private user-read-email",
+    redirect_uri: "localhost:3000/callback",
+    state: generateRandomString(16),
+  };
+  const url =
+    "https://accounts.spotify.com/authorize?" + querystring.stringify(params);
+  window.location.href = url;
+}
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
+import redirect_to_spotify from "./api/login";
+
 export default function Home() {
   return (
     <div className = "">
-      <button>
-        <a href="/login">
-        login
-        </a>
-        </button>
+      <button onClick={redirect_to_spotify}>
+        Login With Spotify
+      </button>
     </div>
   )
 }
