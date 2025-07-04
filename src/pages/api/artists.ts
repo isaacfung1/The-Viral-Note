cdee885b4b7958dca57848a08fcf595e58395994
Isaac Fung <fsaaciung@gmail.com>
2025-07-03 22:27:01 -0400

    feat: artist db insertion

--- src/pages/api/artists.ts
+++ src/pages/api/artists.ts
@@ -1,8 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios';
+import pool from '../../lib/dbClient';
 
-export default async function get_user_artists(req: NextApiRequest, res: NextApiResponse) {
-    const user_artists_response = await axios.get('https://api.spotify.com/v1/me/top/artists', {
+export default async function get_artists(req: NextApiRequest, res: NextApiResponse) {
+    const artists_response = await axios.get('https://api.spotify.com/v1/me/top/artists', {
         params: {
             limit: 50,
             time_range: 'long_term',
@@ -13,6 +14,36 @@ export default async function get_user_artists(req: NextApiRequest, res: NextApi
         }
     })  
 
+    if (artists_response.status !== 200) {
+        return res.status(401).json({ error: 'unable to retrieve user top artists'});
+    }
+
+    const artists_data = artists_response.data.items;
+
+    type artist = {
+        id: string;
+        name: string;
+        genres: string[];
+        popularity: number;
+
+    }
+
+    const cleaned_artists_data = artists_data.map((artist:artist) => ({
+        id: artist.id,
+        name: artist.name,
+        genres: artist.genres,
+        popularity: artist.popularity
+    }));
 
 
+    for (const artist of cleaned_artists_data) {
+        const artist_check = await pool.query("SELECT EXISTS (SELECT 1 FROM artists WHERE artist_id = $1)", [artist.id])
+        if (artist_check.rows[0].exists) {
+            continue;
+        }
+        else {
+            await pool.query(`INSERT INTO artists (artist_id, name, genres, popularity) VALUES 
+                ($1, $2, $3, $4)`, [artist.id, artist.name, artist.genres, artist.popularity]);
+        }
+    }
 }
