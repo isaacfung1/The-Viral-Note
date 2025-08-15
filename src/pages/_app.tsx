0f7677a5911f1859505940805835f6aaa15182cf
Isaac Fung <fsaaciung@gmail.com>
2025-08-14 21:21:18 -0400

    fix: deployment error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import Layout from '../components/layout';
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
 
 export default function App({ Component, pageProps}: AppProps) {
     return(
